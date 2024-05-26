a6a637a25b123fbef47de588d7f71725cbf18a56
SabeerJuniad <sabeerjunaid992@gmail.com>
2024-05-26 12:05:24 +0500

    literal values

---
+++ Store and Retrieve Data Using Literal and Variable Values in C#/2. Print literal values.cs
@@ -0,0 +1,12 @@
+Console.WriteLine('b'); // Single Char
+
+Console.WriteLine(123); //Integer
+
+Console.WriteLine(0.25f); //Float
+
+Console.WriteLine(2.255); //Double
+
+Console.WriteLine(12.38942m); //literal sufflix m
+
+Console.WriteLine(true);
+Console.WriteLine(false); //Boolean
