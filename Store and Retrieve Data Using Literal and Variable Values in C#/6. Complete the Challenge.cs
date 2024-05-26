44caf9577332a7cc1df06a9d2536b383749c6d49
SabeerJuniad <sabeerjunaid992@gmail.com>
2024-05-26 12:21:12 +0500

    unit 6

---
+++ Store and Retrieve Data Using Literal and Variable Values in C#/6. Complete the Challenge.cs
@@ -0,0 +1,7 @@
+string name = "Bob";
+int messages = 3;
+float temperature = 34.4f;
+
+Console.Write("Hello,"+name+"!");
+Console.Write(" You have "+messages+" messages in your inbox. ");
+Console.Write("The temperature is "+temperature+" celsius.");
