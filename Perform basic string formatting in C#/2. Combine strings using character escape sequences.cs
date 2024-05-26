21ba4994f83c1e6984e746db8a034f938ff26b46
SabeerJuniad <sabeerjunaid992@gmail.com>
2024-05-26 12:34:21 +0500

    unit 2

---
+++ Perform basic string formatting in C#/2. Combine strings using character escape sequences.cs
@@ -0,0 +1,11 @@
+Console.WriteLine("Hello\nWorld!"); // n to get into new line
+Console.WriteLine("Hello\tWorld!"); // t make a specific amount of space between text just like when you press TAB
+
+//if you want to print output with "
+
+Console.WriteLine("Hello\"World\"!");
+
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
