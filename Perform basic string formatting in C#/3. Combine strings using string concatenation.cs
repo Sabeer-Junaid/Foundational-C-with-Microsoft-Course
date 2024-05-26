e5830d4709a80c4c96e1ef938b9c2507b4952b4c
SabeerJuniad <sabeerjunaid992@gmail.com>
2024-05-26 12:38:39 +0500

    unit 3

---
+++ Perform basic string formatting in C#/3. Combine strings using string concatenation.cs
@@ -0,0 +1,7 @@
+string firstName = "Bob";
+string message = "Hello " + firstName;
+Console.WriteLine(message);
+
+string firstName = "Bob";
+string greeting = "Hello";
+Console.WriteLine(greeting + " " + firstName + "!");
