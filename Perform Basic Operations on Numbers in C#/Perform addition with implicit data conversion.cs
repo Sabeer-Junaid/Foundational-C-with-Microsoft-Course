697f1e8a2036e7fd29f8e3a1b81aa6ae15570703
SabeerJuniad <sabeerjunaid992@gmail.com>
2024-05-27 05:10:23 +0500

    unit 2

---
+++ Perform Basic Operations on Numbers in C#/Perform addition with implicit data conversion.cs
@@ -0,0 +1,7 @@
+int firstNumber = 12;
+int secondNumber = 7;
+Console.WriteLine(firstNumber + secondNumber);
+
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + widgetsSold + " widgets.");
