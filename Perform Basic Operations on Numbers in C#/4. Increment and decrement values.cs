e60536ee2118f08471f019c549b8ffa7a63a5c3f
SabeerJuniad <sabeerjunaid992@gmail.com>
2024-05-27 05:19:07 +0500

    unit 4

---
+++ Perform Basic Operations on Numbers in C#/4. Increment and decrement values.cs
@@ -0,0 +1,21 @@
+
+
+int value = 1;
+
+value = value + 1;
+Console.WriteLine("First increment: " + value);
+
+value += 1;
+Console.WriteLine("Second increment: " + value);
+
+value++;
+Console.WriteLine("Third increment: " + value);
+
+value = value - 1;
+Console.WriteLine("First decrement: " + value);
+
+value -= 1;
+Console.WriteLine("Second decrement: " + value);
+
+value--;
+Console.WriteLine("Third decrement: " + value);
